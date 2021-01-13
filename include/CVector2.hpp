430dcf88063cbb7314c98f4081fcb297cf6515c7
caastilho <alfaadeveloper@gmail.com>
2021-01-13 19:42:32 -0300

    CVector2 fully implemented

--- include/CVector2.hpp
+++ include/CVector2.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <cmath>
 #include <cstdlib>
 #include <time.h>
 
@@ -16,12 +18,53 @@ struct CVector2
     
     // Construct CVector2 environment
     CVector2(double x, double y);
-    CVector2(double scalar);
+    CVector2(const CVector2& source);
     CVector2();
     
     // Get random CVector2
     static CVector2 getRandom(double minimum, double maximum);
     static CVector2 getRandom();
+    
+    // Get 2D vector from angle
+    static CVector2 fromAngle(double angle);
+    
+    
+    ///////////////////////////////
+    // Especial vector functions //
+    ///////////////////////////////
+    
+    // Get the distance between two vectors
+    static double getDistance(const CVector2& a, const CVector2& b);
+    
+    // Get angle between two vectors (in radians)
+    static double getAngleBetween(CVector2& a, CVector2& b);
+    
+    // Get the dot product between two vectors
+    static double dot(const CVector2& a, const CVector2& b);
+    
+    // Get the hadamard product between two vectors
+    static CVector2 hadamard(const CVector2& a, const CVector2& b);
+    
+    
+    // Constrain values between a certain range
+    void constrain(double minimum, double maximum);
+    
+    // Get heading vector representation
+    void normalize();
+    CVector2 getNormalized();
+    double getHeading();
+    
+    // 2D vector rotations
+    void rotate(double angle);
+    CVector2 getRotate(double angle);
+    
+    // Set and get vector's magnitude
+    void setMagnitude(double magnitude);
+    double getMagnitude();
+    
+    // 2D vector multiplication techniques
+    double dot(const CVector2& source);
+    void hadamard(const CVector2& source);
     
 };
 
--- source/CVector2.cpp
+++ source/CVector2.cpp
@@ -12,10 +12,10 @@ CVector2::CVector2(double _x, double _y)
     this->y = _y;
 }
 
-CVector2::CVector2(double scalar)
+CVector2::CVector2(const CVector2& source)
 {
-    this->x = scalar;
-    this->y = scalar;
+    this->x = source.x;
+    this->y = source.y;
 }
 
 CVector2::CVector2()
@@ -38,6 +38,125 @@ CVector2 CVector2::getRandom()
     double new_x = (double)rand() / RAND_MAX;
     double new_y = (double)rand() / RAND_MAX;
     return CVector2(new_x, new_y);
+}
+
+
+// Get 2D vector from angle (-pi to pi)
+CVector2 CVector2::fromAngle(double angle)
+{
+    float new_x = cos(angle);
+    float new_y = sin(angle);
+    return CVector2(new_x, new_y); 
+}
+
+
+
+///////////////////////////////
+// Especial vector functions //
+///////////////////////////////
+
+// Get the distance between two vectors
+double CVector2::getDistance(const CVector2& a, const CVector2& b)
+{
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+// Get angle between two vectors (in radians)
+double CVector2::getAngleBetween(CVector2& a, CVector2& b)
+{
+    return acos(CVector2::dot(a, b) / (a.getMagnitude() * b.getMagnitude()));
+}
+
+// Get the dot product between two vectors
+double CVector2::dot(const CVector2& a, const CVector2& b)
+{
+    return (a.x * b.x) + (a.y * b.y); 
+}
+
+// Get the hadamard product between two vectors
+CVector2 CVector2::hadamard(const CVector2& a, const CVector2& b)
+{
+    double new_x = a.x * b.x;
+    double new_y = a.y * b.y;
+    return CVector2(new_x, new_y); 
+}
+
+
+// Constrain values between a certain range
+void CVector2::constrain(double minimum, double maximum)
+{
+    this->x = std::max(minimum, std::min(maximum, this->x));
+    this->y = std::max(minimum, std::min(maximum, this->y));
+}
+
+
+// Get heading vector representation
+void CVector2::normalize()
+{
+    double magnitude = this->getMagnitude();
+    this->x /= magnitude;
+    this->y /= magnitude;
+}
+
+CVector2 CVector2::getNormalized()
+{
+    double magnitude = this->getMagnitude();
+    double new_x = this->x / magnitude;
+    double new_y = this->y / magnitude; 
+    
+    return CVector2(new_x, new_y);
+}
+
+double CVector2::getHeading()
+{
+    return atan2(this->y, this->x);
+}
+
+
+// 2D vector rotations
+void CVector2::rotate(double angle)
+{
+    double cosValue = cos(angle); 
+    double sinValue = sin(angle);
+    this->x = this->x * cosValue - this->y * sinValue;
+    this->y = this->x * sinValue + this->y * cosValue;
+}
+
+CVector2 CVector2::getRotate(double angle)
+{
+    double cosValue = cos(angle); 
+    double sinValue = sin(angle);
+    
+    double new_x = this->x * cosValue - this->y * sinValue;
+    double new_y = this->x * sinValue + this->y * cosValue;
+    return CVector2(new_x, new_y);
+}
+
+
+// Set and get vector's magnitude
+void CVector2::setMagnitude(double magnitude)
+{
+    double ratio = magnitude / this->getMagnitude();
+    this->x *= ratio;
+    this->y *= ratio;
+}
+
+double CVector2::getMagnitude()
+{
+    return sqrt(pow(this->x, 2) + pow(this->y, 2));
+}
+
+
+// 2D vector multiplication techniques
+double CVector2::dot(const CVector2& source)
+{
+    return (this->x * source.x) + (this->y * source.y);
+}
+
+void CVector2::hadamard(const CVector2& source)
+{
+    this->x *= source.x;
+    this->y *= source.y;
 }
 
 
